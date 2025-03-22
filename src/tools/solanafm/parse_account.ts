e4c01b5ae756f90a1626662180e041b68a9b3f63
resister-boy <hassanpumped17@gmail.com>
2025-03-22 17:37:17 +0900

    feat: declare parse_account, parse_instruction function as a tool

---
+++ src/tools/solanafm/parse_account.ts
@@ -0,0 +1,45 @@
+import {
+  checkIfAccountParser,
+  ParserType,
+  SolanaFMParser,
+} from "@solanafm/explorer-kit";
+import { getProgramIdl, IdlItem } from "@solanafm/explorer-kit-idls";
+import { AccountParserResponse } from "../../types";
+
+/**
+ * Parse a Solana account data
+ * @param program_id Solana program ID
+ * @param account_data Solana account data
+ * @returns Object containing the program name, account data layout, parsed account data
+ */
+
+export async function parse_account(
+  program_id: string,
+  account_data: string,
+): Promise<AccountParserResponse> {
+  try {
+    const idl: IdlItem | null = await getProgramIdl(program_id);
+
+    if (!idl) {
+      throw new Error(`Error fetching IDL for program ${program_id}`);
+    }
+
+    const accountParser = new SolanaFMParser(idl, program_id).createParser(
+      ParserType.ACCOUNT,
+    );
+
+    if (!accountParser || !checkIfAccountParser(accountParser)) {
+      throw new Error(
+        `Error creating account parser for program ${program_id}`,
+      );
+    }
+
+    return {
+      name: accountParser.getProgramName(),
+      layout: accountParser.accountLayouts,
+      data: accountParser.parseAccount(account_data),
+    };
+  } catch (error) {
+    throw new Error(`Error failed to parse account: ${error}`);
+  }
+}
